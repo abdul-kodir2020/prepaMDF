137d204d206a229ddcea875c9db3479d2113e19d
abdul-kodir2020 <62393812+abdul-kodir2020@users.noreply.github.com>
2025-02-03 15:20:38 +0100

    Update MDF - Round Basket - 2.js

--- 2023/MDF - Round 3 -13h30/MDF - Round Basket - 2.js
+++ 2023/MDF - Round 3 -13h30/MDF - Round Basket - 2.js
@@ -35,3 +35,27 @@ function ContestResponse(){
     
     isThere ? field.map(el => el.join('')).forEach(el => console.log(el)) : console.log('Impossible')
 }
+
+
+/*
+ça marche avec ce code mais l'exécuteur affiche toujours impossible à la fin ( Néanmoins ça resout le problème ) du coup j'ai rajouté une variable isThere ( en haut ) pour éviter ça
+function ContestResponse(){
+    //implement your code here using input array
+    const [n, m] = input[0].split(' ').map(el => parseInt(el))
+    const field = input.slice(- n).map(el => el.split(''))
+    
+    for(let i = 0; i< n; i++){
+        for (var j = 0; j < m; j++) {
+            if(field[i][j] === '.' && i < n - 1 && j < m - 1 && field[i][j + 1] === '.' && field[i + 1][j] === '.' && field[i + 1][j + 1] === '.'){
+                field[i][j] = 'O'
+                field[i][j + 1] = 'O'
+                field[i + 1][j] = 'O'
+                field[i + 1][j + 1] = 'O'
+                field.map(el => el.join('')).forEach(el => console.log(el))
+            }
+        }
+    }
+    
+    console.log('Impossible')
+}
+*/
